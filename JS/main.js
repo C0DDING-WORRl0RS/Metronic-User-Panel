85d68ada0e7ef4691d22c2ac924c3c7f8302e52f
fari-panisher <farhad.13ahmadi.83@gmail.com>
2023-12-12 15:32:13 +0330

    refactor (header) :final part
    Des :
    + add navbar for right item in header + slider for small width

--- JS/main.js
+++ JS/main.js
@@ -10,23 +10,32 @@ console.log(icons);
  * when user touchend in moball show this modal
  */
 icons.forEach(function (item) {
-    item.addEventListener('touchend', () => {
-        Swal.fire({
-            title: "در حال ساخت...",
-            showClass: {
-                popup: `
+  item.addEventListener('touchend', () => {
+    Swal.fire({
+      title: "در حال ساخت...",
+      showClass: {
+        popup: `
                 animate__animated
                 animate__fadeInUp
                 animate__faster
               `
-            },
-            hideClass: {
-                popup: `
+      },
+      hideClass: {
+        popup: `
                 animate__animated
                 animate__fadeOutDown
                 animate__faster
               `
-            }
-        });
+      }
     });
+  });
 })
+
+// for responsive mode in right item if header 
+$(document).ready(function () {
+  // Check if the window width is less than a certain value (e.g., 768 pixels)
+  if ($(window).width() > 779) {
+    // Disable the accordion functionality
+    $('.accordion-right').removeClass('accordion');
+  }
+});
