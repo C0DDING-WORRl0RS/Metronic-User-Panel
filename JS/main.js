0c31dc9429d5e6719bada70765b98e10a7592de4
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2023-12-16 02:49:49 +0330

    Implement dynamic display for Scroll to Top button based on scroll position
    Introduced JavaScript functionality to control the visibility and style of the Scroll to Top button. This button becomes visible when the user scrolls down beyond 100 pixels from the top of the page. The display logic ties into the window's 'scroll' event, dynamically adjusting the display and opacity of the '.scrolltop' class element.
    
    Key changes:
    - Added a 'showDivOnScroll' function to toggle the display of the scroll icon based on the page's scroll position.
    - Set a trigger height of 100 pixels as the threshold for the icon's visibility.
    - Bound 'mouseover' and 'mouseout' event listeners to the scroll icon to modify the background color and opacity, enhancing user interaction cues.
    - Utilized 'window.scrollY' to check the current vertical position of the window and show/hide the scroll button accordingly, which will help with improved user navigation and UX.
    
    This commit ensures that the scroll button is only visible when needed, reducing clutter and maintaining a clean interface while providing a user-friendly way to return to the top of the page.

--- JS/main.js
+++ JS/main.js
@@ -37,3 +37,42 @@ $(document).ready(function () {
     $(".accordion-right").removeClass("accordion");
   }
 });
+
+
+
+
+// scroll icon ----------------------
+
+// Get the div that you want to show
+var scrollIcon = document.querySelector('.scrolltop');
+
+// Function to show the div with flex display when scrolled down
+function showDivOnScroll() {
+  var triggerHeight = 100; // The scroll position (in pixels) at which you want to show the div
+  
+  if (window.scrollY > triggerHeight) {
+    // When scrolled down past triggerHeight, change display to flex
+    scrollIcon.style.display = 'flex';
+    scrollIcon.style.opacity = '0.3';
+  } else {
+    // Hide the div when above triggerHeight
+    scrollIcon.style.display = 'none';
+    scrollIcon.style.opacity = '0';
+
+  }
+}
+scrollIcon.addEventListener('mouseover', function() {
+  // Change style when the mouse enters the element area
+  this.style.backgroundColor = '#2C9AFF'; // Put your desired hover style changes here
+  this.style.opacity = '1';
+});
+
+// Define the mouseout event listener
+scrollIcon.addEventListener('mouseout', function() {
+  // Revert style when the mouse leaves the element area
+  this.style.backgroundColor = ''; // Revert back to original styles
+  this.style.opacity = '0.3';
+});
+
+// Add the scroll event listener to the window
+window.addEventListener('scroll', showDivOnScroll);
