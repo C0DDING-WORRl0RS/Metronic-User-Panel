626aa8c153c11d8652d3b92162d95f17ef24ad9d
alireza <121108490+alireza64ahmadi@users.noreply.github.com>
2023-12-16 03:00:20 +0330

    function updateMarginOnScroll() {   var newMarginBottom = '-15px'; // New margin-bottom value when scrolled    // Check if we have scrolled (this is an example, you might need to check against a specific value)   if (window.pageYOffset > 0) { // You can change '0' to any other number depending on when you want the style to apply     scrollIcon.style.marginBottom = newMarginBottom;   } else {     scrollIcon.style.marginBottom = '0'; // Reset to original value when not scrolled   } }  // Listen to the scroll event window.addEventListener('scroll', updateMarginOnScroll);
    Implemented an on-scroll event listener that dynamically changes the bottom margin of the scroll icon, enhancing the UX during page navigation.
    
    Changes include:
    - Addition of 'updateMarginOnScroll' function that applies a margin-bottom of -15px to the scroll icon when the page has been scrolled down from the top.
    - The function checks the 'window.pageYOffset' value to determine the scroll position and updates the style accordingly.
    - A scroll event listener has been added to the window to trigger this function whenever the user scrolls.
    
    This modification helps in preventing layout shifts or overlapping with other page elements when the user scrolls down, keeping the interface clean and visually appealing.

--- JS/main.js
+++ JS/main.js
@@ -45,6 +45,8 @@ $(document).ready(function () {
 
 // Get the div that you want to show
 var scrollIcon = document.querySelector('.scrolltop');
+var header = document.querySelector("#main-header")
+scrollIcon.href = header
 
 // Function to show the div with flex display when scrolled down
 function showDivOnScroll() {
@@ -76,3 +78,22 @@ scrollIcon.addEventListener('mouseout', function() {
 
 // Add the scroll event listener to the window
 window.addEventListener('scroll', showDivOnScroll);
+
+
+
+
+
+// Function that updates the style
+function updateMarginOnScroll() {
+  var newMarginBottom = '-15px'; // New margin-bottom value when scrolled
+
+  // Check if we have scrolled (this is an example, you might need to check against a specific value)
+  if (window.pageYOffset > 0) { // You can change '0' to any other number depending on when you want the style to apply
+    scrollIcon.style.marginBottom = newMarginBottom;
+  } else {
+    scrollIcon.style.marginBottom = '0'; // Reset to original value when not scrolled
+  }
+}
+
+// Listen to the scroll event
+window.addEventListener('scroll', updateMarginOnScroll);
